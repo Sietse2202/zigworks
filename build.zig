9296da17a3ab70c02bb5e172049570dfc02638af
Sietse <sietsevanderhoeven@gmail.com>
2026-04-06 00:33:17 +0200

    Fix modules in build.zig

--- build.zig
+++ build.zig
@@ -4,24 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const root_module = b.addModule("eadk", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const lib = b.addLibrary(.{
         .name = "zigworks",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = root_module,
     });
 
     b.installArtifact(lib);
 
     const unit_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = root_module,
     });
 
     const test_step = b.step("test", "Run library unit tests");
@@ -30,11 +28,7 @@ pub fn build(b: *std.Build) void {
     const docs_lib = b.addLibrary(.{
         .name = "zigworks_docs",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = root_module,
     });
 
     const install_docs = b.addInstallDirectory(.{
