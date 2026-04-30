aa08b56b55c8d4ba47d77df8fa6094536c711d77
Sietse <sietsevanderhoeven@gmail.com>
2026-04-30 16:43:06 +0200

    Remove docs-serve

--- build.zig
+++ build.zig
@@ -39,20 +39,4 @@ pub fn build(b: *std.Build) void {
 
     const docs_step = b.step("docs", "Generate HTML documentation");
     docs_step.dependOn(&install_docs.step);
-
-    const serve_exe = b.addExecutable(.{
-        .name = "serve_docs",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("tools/serve_docs.zig"),
-            .target = b.graph.host,
-            .optimize = .Debug,
-        }),
-    });
-
-    const run_server = b.addRunArtifact(serve_exe);
-    run_server.setCwd(b.path("zig-out/docs"));
-    run_server.step.dependOn(&install_docs.step);
-
-    const serve_step = b.step("docs-serve", "Build docs and serve on localhost");
-    serve_step.dependOn(&run_server.step);
 }
